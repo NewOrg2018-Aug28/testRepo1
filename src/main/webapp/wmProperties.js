cb6466740db1665e41564096a88a486bc198a388
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-07-05 05:26:15 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "clear-glass",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "SalesVision_updated",
+  "homePage" : "Main",
+  "name" : "SalesVision_updated",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
